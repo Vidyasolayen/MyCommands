cafd1e23d6fee7248ee14e78513afef9b0771c2b
Gianeeshwar Mohun <Ashley.Mohun@ceridian.com>
2019-08-01 14:27:35 +0400

    majorChanges

--- MyCommands/Configuration.Designer.cs
+++ MyCommands/Configuration.Designer.cs
@@ -48,6 +48,8 @@
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.Size = new System.Drawing.Size(536, 197);
             this.dataGridView1.TabIndex = 41;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            this.dataGridView1.SelectionChanged += new System.EventHandler(this.dataGridView1_SelectionChanged);
             // 
             // label18
             // 
---
+++ MyCommands/MyCommandsDAL.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace MyCommands
+{
+    public class MyCommandsDAL
+    {
+        public string getVersionPath(string versionNumber)
+        {
+            SqlConnection sqlConnection1 = new SqlConnection("Integrated Security=SSPI;Persist Security Info=False;User ID=wbpoc;Initial Catalog=DFCommands;Data Source=.");
+            SqlCommand cmd = new SqlCommand();
+            SqlDataReader reader;
+
+            cmd.CommandText = $"SELECT VersionPath FROM tblVersion where versionNumber='{versionNumber}'";
+            cmd.CommandType = CommandType.Text;
+            cmd.Connection = sqlConnection1;
+
+            sqlConnection1.Open();
+
+            reader = cmd.ExecuteReader();
+
+            string results = "";
+
+            // Data is accessible through the DataReader object here..
+            if (reader.Read())
+            {
+                results = reader["VersionPath"].ToString();
+            }
+
+            sqlConnection1.Close();
+            return results;
+        }
+
+        public List<int> getAllVersionNumbers()
+        {
+            SqlConnection con = new SqlConnection("Integrated Security=SSPI;Persist Security Info=False;User ID=wbpoc;Initial Catalog=DFCommands;Data Source=.");
+            SqlCommand command = new SqlCommand("select versionNumber from tblVersion", con);
+            List<int> ints = new List<int>();
+            con.Open();
+
+            using (SqlDataReader reader = command.ExecuteReader())
+            {
+                while (reader.Read())
+                {
+                    ints.Add(reader.GetInt32(0)); // provided that first (0-index) column is int which you are looking for
+                }
+            }
+
+            con.Close();
+            return ints;
+        }
+
+    }
+}
